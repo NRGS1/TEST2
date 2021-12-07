ca5ecdba6ca190d1b4ab44bcfedc208c18971acb
NRGS1 <95704993+NRGS1@users.noreply.github.com>
2021-12-07 16:16:53 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,206 @@
+import '@ar-js-org/ar.js';
+import 'aframe-look-at-component';
+import 'aframe-osm-3d';
+
+
+AFRAME.registerComponent('poifinder', {
+
+    init: function() {
+
+
+        this.camera = document.querySelector('a-camera');
+
+        // Handle a GPS update ...
+        window.addEventListener('gps-camera-update-position', e => {
+            console.log(`pos ${e.detail.position.latitude} ${e.detail.position.longitude}`);
+            
+            //Set the latitude and logitude and download the DEM data
+            console.log('Calling terrarium dem..');
+            this.el.setAttribute('terrarium-dem', {
+                lat: e.detail.position.latitude,
+                lon: e.detail.position.longitude 
+            })
+        });
+
+        // the OSM-DATA-LOADED event will fire when the OSM data has been downloaded.
+        this.el.addEventListener('osm-data-loaded', e => {
+
+
+            //the e.detail.pois have Geojson stored and it will 
+            //be use to filter the results to project the features specified
+            e.detail.pois
+            //the filter function will search all the POIS that contains the cafe property
+                .filter ( poi => poi.properties.amenity == 'cafe')
+                .forEach ( cafe => {
+
+                    //creates an entity for the text and gltf model and a compound entity which it will contain
+                    //the text and gltf model
+                    const textEntity = document.createElement('a-text');
+                    const coffeentity = document.createElement('a-entity');
+                    const compoundEntity = document.createElement('a-entity');
+
+                    //Import the model and will specify the size and the position
+                    coffeentity.setAttribute('gltf-model', '#coffee');
+
+                    coffeentity.setAttribute('position', {
+                        x: 0,
+                        y: 70,
+                        z: 0
+                    });
+
+                    coffeentity.setAttribute('scale',  {
+                        x: 20,
+                        y: 20,
+                        z: 20
+                    });
+
+                    //set the compound entity on the lat log of the poi
+                    compoundEntity.setAttribute('position', {    
+                        x: 0,
+                        y: cafe.geometry.coordinates[2],
+                        z: 0
+                    });
+
+                    // set the lat and lon of the compound entity by setting
+                    // its gps-projected-entity place component
+                    compoundEntity.setAttribute('gps-projected-entity-place', {
+                        latitude: cafe.geometry.coordinates[1],
+                        longitude: cafe.geometry.coordinates[0]
+                    });
+
+                    // Set the text scale
+                    textEntity.setAttribute('scale',  {
+                        x: 200,
+                        y: 200,
+                        z: 200
+                    });
+
+                    // Make the model and text to look-at the camera
+                    coffeentity.setAttribute('look-at', '[gps-projected-camera]');
+                    textEntity.setAttribute('look-at', '[gps-projected-camera]');
+
+    
+                    
+                    // text is placed at the parent entity's world position
+                    // if the poi do not have a name it will display 'No name'
+                    textEntity.setAttribute('align', 'center');
+                    textEntity.setAttribute('value', cafe.properties.name || 'No name');
+
+                    // The values that be parsed after the user clicks on the POI
+                    coffeentity.setAttribute("clicker", {
+                        name: cafe.properties.name,
+                        website: cafe.properties.website
+                    });
+
+                    //add the text and model elements inside the compound entity
+                    compoundEntity.appendChild(textEntity);
+                    compoundEntity.appendChild(coffeentity);
+                    
+                    // Add the compound entity to the scene
+                    this.el.sceneEl.appendChild(compoundEntity);
+            });
+
+            //the e.detail.pois have Geojson stored and it will 
+            //be use to filter the results to project the features specified
+            e.detail.pois
+            //the filter function will search all the POIS that contains the restaurant property
+            .filter ( poi => poi.properties.amenity == 'restaurant')
+            .forEach ( restaurant => {
+
+                //creates an entity for the text and gltf model and a compound entity which it will contain
+                //the text and gltf model
+                const textEntity = document.createElement('a-text');
+                const foodentity = document.createElement('a-entity');
+                const compoundEntity = document.createElement('a-entity');
+                    
+                //Import the model and will specify the size and the position
+                foodentity.setAttribute('gltf-model', '#food');
+
+                foodentity.setAttribute('position', {
+                    x: 0,
+                    y: 70,
+                    z: 0
+                });
+
+                foodentity.setAttribute('scale',  {
+                    x: 100,
+                    y: 100,
+                    z: 100
+                });
+
+                //set the compound entity on the lat log of the poi
+                compoundEntity.setAttribute('position', {    
+                    x: 0,
+                    y: restaurant.geometry.coordinates[2],
+                    z: 0
+                });
+
+                // set the lat and lon of the compound entity by setting
+                // its gps-projected-entity place component
+                compoundEntity.setAttribute('gps-projected-entity-place', {
+                    latitude: restaurant.geometry.coordinates[1],
+                    longitude: restaurant.geometry.coordinates[0]
+                });
+
+                // Set the text scale
+                textEntity.setAttribute('scale',  {
+                    x: 200,
+                    y: 200,
+                    z: 200
+                });
+                // Make the model and text to look-at the camera
+                foodentity.setAttribute('look-at', '[gps-projected-camera]')
+                textEntity.setAttribute('look-at', '[gps-projected-camera]');
+
+
+                 // text is placed at the parent entity's world position
+                 // if the poi do not have a name it will display 'No name'
+                textEntity.setAttribute('align', 'center');
+                textEntity.setAttribute('value', restaurant.properties.name || 'No name');
+
+                // The values that be parsed after the user clicks on the POI
+                foodentity.setAttribute("clicker", {
+                    name: restaurant.properties.name,
+                    website: restaurant.properties.website
+                });
+
+                //add the text and model elements inside the compound entity
+                compoundEntity.appendChild(foodentity);
+                compoundEntity.appendChild(textEntity);
+                
+                // Add the compound entity to the scene
+                this.el.sceneEl.appendChild(compoundEntity);
+        });
+        });
+    }
+});
+
+
+AFRAME.registerComponent('clicker', {
+    // Schema - allowing name and website to be passed in 
+    schema: {
+        name: {
+            type: 'string',
+            default: '',
+        },
+        website: {
+            type: 'string',
+            default: '',
+        }
+    },
+    init: function() {
+        this.el.addEventListener('click', e=> {
+
+            //check if the Poi have a website
+            if(`${this.data.website}` == ''){
+                alert(`${this.data.name}`+' do not have a website incorpored. Cancel will load this website ');
+            }else{
+            if (window.confirm('If you click "ok" you would be redirected to '+`${this.data.name}`+' website. If you click Cancel you will continue on this page')) {
+                window.location.href = `${this.data.website}`;
+             };
+
+            }
+        });
+    }
+    
+});
